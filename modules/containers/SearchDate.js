56d91c018ca65c46d2c0c217cccbdda61073bd2d
Michael Musil <infi.pc@gmail.com>
2014-12-09 22:03:18 +0100

    SearchDate getters (not tested)

--- modules/containers/SearchDate.js
+++ modules/containers/SearchDate.js
@@ -3,6 +3,8 @@ var moment = require("moment");
 var urlDateFormat = "YYYY-MM-DD";
 
 /*
+class SearchDate
+it has state for all modes, bude for
   constructor
   input = plain object or string or just another SearchDate object
  */
@@ -21,6 +23,65 @@ SearchDate = function (input) {
   this.final = true;
 };
 
+/* sets mode with checking some internal validity */
+/* but probably valitity should be handled by getters, because it is nice to have internal state for others modes than enabled */
+
+//SearchDate.prototype.setMode = function(mode) {
+//  var previousMode = this.mode;
+//  if (mode == "single") {
+//    this.to = this.from;
+//  } else if (mode == "anytime") {
+//    this.from = moment.utc().add(1, "days");
+//    this.to = moment.utc().add(6, "months");
+//  }
+//  this.mode = mode;
+//};
+
+
+
+SearchDate.prototype.getMode = function(mode) {
+  return this.mode
+};
+
+SearchDate.prototype.getFrom = function(mode) {
+  if (this.mode == "timeToStay" || this.mode == "noReturn") {
+    return null;
+  } else if (this.mode == "anytime") {
+    return moment.utc().add(1,"days");
+  } else {
+    return this.from
+  }
+};
+
+SearchDate.prototype.getTo = function(mode) {
+  if (this.mode == "timeToStay" || this.mode == "noReturn") {
+    return null;
+  } else if (this.mode == "single") {
+    return this.from
+  } else if (this.mode == "anytime") {
+    return moment.utc().add(6,"months");
+  } else {
+    return this.to
+  }
+};
+
+SearchDate.prototype.getMinStayDays = function(mode) {
+  if (this.mode == "timeToStay") {
+    return this.minStayDays;
+  } else {
+    return null;
+  }
+};
+
+SearchDate.prototype.getMaxStayDays = function(mode) {
+  if (this.mode == "timeToStay") {
+    return this.maxStayDays;
+  } else {
+    return null;
+  }
+};
+
+/* wa url */
 SearchDate.prototype.toUrlString = function() {
   return this.mode + "_" + this.from.format(urlDateFormat) + "_" + this.to.format(urlDateFormat);
 };
