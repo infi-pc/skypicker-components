ac6491bee53444c14c054099754bb995e84a74c6
Michael Musil <infi.pc@gmail.com>
2014-12-14 18:55:59 +0100

    simplify DatePairValidator

--- modules/DatePicker/DatePairValidator.js
+++ modules/DatePicker/DatePairValidator.js
@@ -1,46 +1,17 @@
-class DatePairValidator {
-  constructor() {
 
+exports.validate = function(outbound, inbound) {
+  if (!outbound) {
+    return "outboundNotSelected"
   }
-  registerInbound(func) {
-    this.getInbound = func;
-  }
-  registerOutbound(func) {
-    this.getOutbound = func;
+  if (!inbound) {
+    return "inboundNotSelected"
   }
 
-  /*
-  validates potential new value according to previous values
-   */
-  validate(direction, value) {
-    var outbound, inbound;
-    if (direction == "inbound") {
-      inbound = value;
-      outbound = this.getOutbound();
-    } else if (direction == "outbound") {
-      inbound = this.getInbound();
-      outbound = value;
-    } else {
-      inbound = this.getInbound();
-      outbound = this.getOutbound();
+  if (inbound.mode == "single" && outbound.mode == "single") {
+    if (inbound.getDate().format("YYYYMMDD") < outbound.getDate().format("YYYYMMDD")) {
+      return "crossedDates"
     }
-
-    if (!outbound) {
-      return ["outboundNotSelected"]
-    }
-    if (!inbound) {
-      return ["inboundNotSelected"]
-    }
-
-    if (inbound.mode == "single" && outbound.mode == "single") {
-      if (inbound.getDate().format("YYYYMMDD") < outbound.getDate().format("YYYYMMDD")) {
-        return ["crossedDates"]
-      }
-    }
-
-    /* TODO Validate also interval searching */
-    return null;
   }
-}
+  return null;
+};
 
-module.exports = DatePairValidator;
--- tests/units/datePairValidator.js
+++ tests/units/datePairValidator.js
@@ -1,39 +1,29 @@
 var SearchDate = require("../../modules/containers/SearchDate.js");
-var DatePairValidator = require("../../modules/DatePicker/DatePairValidator.js");
+var validator = require("../../modules/DatePicker/DatePairValidator.js");
 
 
 describe("validating", function() {
-  var simpleModel = {
-    outbound: null,
-    inbound: null
-  };
-  var validator = new DatePairValidator();
-
-  validator.registerInbound(function () {
-    return simpleModel.inbound
-  });
-  validator.registerOutbound(function () {
-    return simpleModel.outbound
-  });
   describe("when outbound is single, inbound should cause", function() {
-    var errors;
-    simpleModel.outbound = new SearchDate({mode: "single", from: moment().add(2, "days")});
-    simpleModel.inbound = null;
+    var error, outbound, inbound;
     it("no error", function () {
-      errors = validator.validate("inbound", new SearchDate({mode: "single", from: moment().add(9, "days")}));
-      expect(errors).toBe(null);
+      outbound = new SearchDate({mode: "single", from: moment().add(1, "days")});
+      inbound = new SearchDate({mode: "single", from: moment().add(9, "days")});
+      expect(validator.validate(outbound, inbound)).toBe(null);
     });
     it("error on crossed dates", function () {
-      errors = validator.validate("inbound", new SearchDate({mode: "single", from: moment().add(0, "days")}));
-      expect(errors[0]).toBe("crossedDates");
+      outbound = new SearchDate({mode: "single", from: moment().add(5, "days")});
+      inbound = new SearchDate({mode: "single", from: moment().add(1, "days")});
+      expect(validator.validate(outbound, inbound)).toBe("crossedDates");
     });
     it("ok when no return", function () {
-      errors = validator.validate("inbound", new SearchDate({mode: "noReturn"}));
-      expect(errors).toBe(null);
+      outbound = new SearchDate({mode: "single", from: moment().add(5, "days")});
+      inbound = new SearchDate({mode: "noReturn"});
+      expect(validator.validate(outbound, inbound)).toBe(null);
     });
     it("error because should be selected", function () {
-      errors = validator.validate("inbound", null);
-      expect(errors[0]).toBe("inboundNotSelected");
+      outbound = new SearchDate({mode: "single", from: moment().add(5, "days")});
+      inbound = null;
+      expect(validator.validate(outbound, inbound)).toBe("inboundNotSelected");
     });
 
   });
