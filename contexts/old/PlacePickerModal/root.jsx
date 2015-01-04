510adcd9dc0b36708fb8a01e59afc659827795a6
Michael Musil <infi.pc@gmail.com>
2015-01-04 16:51:32 +0100

    remove old context system

--- contexts/old/PlacePickerModal/root.jsx
+++
@@ -1,7 +0,0 @@
-
-translationStrategy = require('./../../modules/translationStrategies/waTr.js');
-
-var tr = require('./../../modules/tr.js');
-tr.setStrategy(translationStrategy);
-
-window.PlacePickerModal = require('./../../modules/PlacePicker/PlacePickerModal.js');
--- contexts/old/WhichairlineBundle/root.jsx
+++
@@ -1,5 +0,0 @@
-window.DatePickerModal = require('./../../modules/DatePicker/DatePickerModal.jsx');
-window.SearchDate = require('./../../modules/containers/SearchDate.js');
-window.DealsList = require('./../../modules/DealsList.jsx');
-window.SkypickerAPIv1 = require('./../../modules/SkypickerAPIv1.js');
-window.SkypickerTools = require('./../../modules/SkypickerTools.js');
