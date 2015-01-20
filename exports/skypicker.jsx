a0c5189d8fe421e908be888234ff94b534b9dcb0
Michael Musil <infi.pc@gmail.com>
2015-01-20 12:25:15 +0100

    nemespace

--- exports/skypicker.jsx
+++ exports/skypicker.jsx
@@ -3,10 +3,13 @@ var tr = require('./../modules/tr.js');
 tr.setStrategy(translationStrategy);
 var React = require("react");
 
-window.Place = require('./../modules/containers/Place.jsx');
-window.SearchDate = require('./../modules/containers/SearchDate.jsx');
-window.SearchPlace = require('./../modules/containers/SearchPlace.jsx');
-window.SearchFormData = require('./../modules/containers/SearchFormData.jsx');
-window.SearchFormStore = require('./../modules/stores/SearchFormStore.jsx');
-window.SearchForm = require('./../modules/plainJsAdapters/SearchFormAdapter.jsx');
+window.Wgts = {}; //That's namespace if there will be some name collision
+
+window.Place = Wgts.Place = require('./../modules/containers/Place.jsx');
+window.Radius = Wgts.Radius = require('./../modules/containers/Radius.jsx');
+window.SearchDate = Wgts.SearchDate = require('./../modules/containers/SearchDate.jsx');
+window.SearchPlace = Wgts.SearchPlace = require('./../modules/containers/SearchPlace.jsx');
+window.SearchFormData = Wgts.SearchFormData = require('./../modules/containers/SearchFormData.jsx');
+window.SearchFormStore = Wgts.SearchFormStore = require('./../modules/stores/SearchFormStore.jsx');
+window.SearchForm = Wgts.SearchForm = require('./../modules/plainJsAdapters/SearchFormAdapter.jsx');
 
