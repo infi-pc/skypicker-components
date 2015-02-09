6dbc149c7e658a443d9a1c96b82f4567e23d4931
Michael Musil <infi.pc@gmail.com>
2015-02-09 15:06:22 +0100

    adjust styles

--- modules/Map/PlaceLabel.jsx
+++ modules/Map/PlaceLabel.jsx
@@ -22,17 +22,31 @@ var PlaceLabel = React.createClass({
     var mapPlace = this.props.label.mapPlace;
     var style = this.props.style;
     var fullLabel, image, price;
+    var labelText = mapPlace.place.shortName;
+    var className = "city-label";
+    var flagText = "";
     if (mapPlace.price) {
       var priceStyle = {
-        color: "hsla("+parseInt( (1-this.props.label.relativePrice) *115)+", 100%, 50%, 1)",
-        textShadow: "0px 1px 1px #000, 1px 0px 1px #000, 1px 1px 1px #000, 1px 0px 1px #000"
+        color: "hsla("+parseInt( (1-this.props.label.relativePrice) *115)+", 100%, 35%, 1)",
+        textShadow: "-1px 1px 1px #fff, 1px -1px 1px #fff, 1px 1px 1px #fff, -1px -1px 1px #fff",
+        fontWeight: "bold"
       };
       price = <span style={priceStyle}>{mapPlace.price}EUR</span>
     }
+
+    if (mapPlace.flag == "origin") {
+      flagText = <span className="flag-text">From: </span>;
+      className += " flag-"+mapPlace.flag;
+    }
+    if (mapPlace.flag == "destination") {
+      flagText = <span className="flag-text">To: </span>;
+      className += " flag-"+mapPlace.flag;
+    }
+
     if (this.props.label.showFullLabel) {
       fullLabel = (
         <div>
-          <span className="city-label-title">{mapPlace.place.shortName}</span><br/>
+          <span className="city-label-title">{flagText} {labelText}</span><br/>
           {price}
         </div>
       );
@@ -40,14 +54,17 @@ var PlaceLabel = React.createClass({
     } else {
       image = <img src="/images/markers/citySmall.png" />
     }
+
+
     if (mapPlace.flag == "origin") {
       image = <img src="/images/markers/cityWithPrice.png" />
     }
     if (mapPlace.flag == "destination") {
       image = <img src="/images/markers/cityWithPrice.png" />
     }
+
     return (
-      <div ref="label" style={style} className="city-label" onClick={this.onClick}>
+      <div ref="label" style={style} className={className} onClick={this.onClick}>
         {image}
         {fullLabel}
       </div>
